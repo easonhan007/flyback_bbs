2a68827b23dfcdefd55f6b535006429b241df0eb
easonhan <nbkhic@qq.com>
2014-02-15 12:20:49 +0800

    fix bug of result finished time

--- models/test_result.rb
+++ models/test_result.rb
@@ -2,4 +2,9 @@ class TestResult < ActiveRecord::Base
 	has_many :answers
 	belongs_to :account
 	belongs_to :test
+
+	def finished_at
+		answers.order('updated_at DESC').first.updated_at
+	end
+
 end
